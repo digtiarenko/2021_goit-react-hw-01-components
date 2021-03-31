0eba54af4dd0f17eb82988f846dae134f0a8b3f7
Valentyn Digtiarenko <baggins999@gmail.com>
2021-03-31 15:21:33 +0300

    Updates

--- static/js/main.07172f8c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],{9:function(t,e,n){"use strict";n.r(e);n(0);var o=n(2),c=n.n(o);var r=function(){return"Hello React"},s=n(3);c.a.render(Object(s.jsx)(r,{}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.07172f8c.chunk.js.map
